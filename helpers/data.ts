339f7edb2a6fcaa40dd17e01e74c78ad496de697
Mihailo Cvetkovic <info@mihailo.cz>
2024-11-27 16:47:37 +0100

    fixed map link

--- helpers/data.ts
+++ helpers/data.ts
@@ -10,4 +10,4 @@ export const CONTACT_EMAIL_LINK = `mailto:${CONTACT_EMAIL}`;
 export const CONTACT_PHONE = '+420 602 205 036';
 export const CONTACT_PHONE_LINK = `tel:${CONTACT_PHONE.split(' ').join('')}`;
 export const CONTACT_ADDRESS = 'Praha - Čertouská 353/8';
-export const CONTACT_MAP_LINK = 'https://maps.app.goo.gl/CQmzhMxfv1bE8fig6';
+export const CONTACT_MAP_LINK = 'https://maps.app.goo.gl/8xjYdXgqhobCYKb79';
