d22e3282f3e2108de9a36ce3941d7c73e92f169a
Mihailo Cvetkovic <info@mihailo.cz>
2025-09-29 21:35:43 +0200

    fonts

--- components/preload-fonts.tsx
+++ components/preload-fonts.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 
 const PRELOAD_FONTS = [
-    '/fonts/poppins/Poppins-Regular.woff2'
+    '/fonts/gloock/Gloock-Regular.woff2'
 ];
 
 export default function PreloadFonts() {
