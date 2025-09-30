60410c23742220a9c80c83bcabef19e5d560007e
Mihailo Cvetkovic <info@mihailo.cz>
2025-09-30 09:55:53 +0200

    favicon

--- helpers/metadata.ts
+++ helpers/metadata.ts
@@ -6,39 +6,27 @@ interface PageMetadata {
     image?: string;
 }
 
-export const APP_NAME = '';
-export const APP_URL = 'https://www.mihailo.cz';
-export const APP_COLOR = '#000';
+export const APP_NAME = 'IPPA';
+export const APP_URL = 'https://koucink.ippa.cz';
+export const APP_COLOR = '#101010';
 
 const TITLE_SUFFIX = APP_NAME;
 const DESCRIPTION = '';
 
-export const DEFAULT_METADATA = {
+export const DEFAULT_METADATA: Metadata = {
     metadataBase: new URL(APP_URL),
     title: formatTitle(APP_NAME),
     description: DESCRIPTION,
     applicationName: APP_NAME,
-    icons: [
-        {
-            url: "/apple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
-        },
-        {
-            url: "/favicon-32x32.png",
-            sizes: "32x32",
-            type: "image/png",
-        },
-        {
-            url: "/favicon-16x16.png",
-            sizes: "16x16",
-            type: "image/png",
-        }
-    ],
     manifest: "/site.webmanifest",
-    other: {
-        "apple-mobile-web-app-title": APP_NAME,
-        "msapplication-TileColor": APP_COLOR,
+    themeColor: APP_COLOR,
+    icons: {
+        icon: [
+            { url: "/favicon.svg", type: "image/svg+xml" },
+            { url: "/favicon.ico" },
+            { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: "/apple-touch-icon.png",
     },
     openGraph: {
         title: formatTitle(APP_NAME),
@@ -47,15 +35,19 @@ export const DEFAULT_METADATA = {
         description: DESCRIPTION,
         type: "website",
         images: [{
-            url: "/images/og-image.jpg",
+            url: `${APP_URL}/images/og-image.jpg`,
         }]
     },
     twitter: {
         card: "summary_large_image",
         title: formatTitle(APP_NAME),
         description: DESCRIPTION,
-        images: ["/images/og-image.jpg"],
-    }
+        images: [`${APP_URL}/images/og-image.jpg`],
+    },
+    other: {
+        "apple-mobile-web-app-title": APP_NAME,
+        "msapplication-TileColor": APP_COLOR,
+    },
 };
 
 function formatTitle(title = '') {
